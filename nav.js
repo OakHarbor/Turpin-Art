3c1ef36c22a12c3aba6b51b23a41ee15dc18b819
Postal06 <ohwdinfo@gmail.com>
2019-09-15 12:57:42 -0700

    overflowY test for side nav

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ function toggleNav() {
     document.getElementById("side-menu").style.display = "block";
     document.getElementById("side-menu").style.width = "60%";
     document.getElementById("side-hidden").style.width = "50%";
-    document.getElementById("noScroll").style.overflow = "hidden";
+    document.getElementById("noScroll").style.overflowY = "hidden";
     document.getElementById("btn-close").style.color = "#000000";
     document.getElementById("btn-close").style.background = "white";
     document.getElementById("on-top").style.display = "block";
@@ -11,7 +11,7 @@ function toggleNav() {
 function closeSideMenu() {
     document.getElementById("side-menu").style.width = "0";
     document.getElementById("side-hidden").style.width = "0";
-    document.getElementById("noScroll").style.overflow = "scroll";
+    document.getElementById("noScroll").style.overflowY = "scroll";
     document.getElementById("btn-close").style.background = "white";
     document.getElementById("btn-close").style.color = "#CE2026";
     document.getElementById("on-top").style.display = "none";
