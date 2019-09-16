e01749c0e390be83170ce90b24b6865210eb862f
Postal06 <ohwdinfo@gmail.com>
2019-09-15 18:52:55 -0700

    test fix fro scrolling page when side menu is active

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ function toggleNav() {
     document.getElementById("side-menu").style.width = "60%";
     document.getElementById("side-hidden").style.width = "50%";
     document.getElementById("noScroll").style.overflow = "hidden";
-    document.getElementById("no-scroll-body").style.height = "100%";
+    document.getElementById("navbar").style.overflow = "hidden";
     document.getElementById("side-menu").style.overflow = "hidden";
     document.getElementById("btn-close").style.color = "#000000";
     document.getElementById("btn-close").style.background = "white";
@@ -14,7 +14,7 @@ function closeSideMenu() {
     document.getElementById("side-menu").style.width = "0";
     document.getElementById("side-hidden").style.width = "0";
     document.getElementById("noScroll").style.overflow = "scroll";
-    document.getElementById("side-menu").style.overflow = "scroll";
+    document.getElementById("navbar").style.overflow = "scroll";
     document.getElementById("btn-close").style.background = "white";
     document.getElementById("btn-close").style.color = "#CE2026";
     document.getElementById("on-top").style.display = "none";
